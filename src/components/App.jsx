1bf739ff70cf909e0ad46f149de922215498928f
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-11-11 08:53:48 +0100

    feat: responsive nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
+import useStyles from "./styles";
 import { Route, Switch } from "react-router-dom";
 import { MovieInfo, Actors, Movie, Profile, Navbar } from "./";
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <Navbar />
-    <main>
-      <Switch>
-        <Route exact path="/"></Route>
-        <Route exact path="/movies/:id">
-          <MovieInfo />
-        </Route>
-        <Route exact path="/actor/:id">
-          <Actors />
-        </Route>
-        <Route exact path="/movies">
-          <Movie />
-        </Route>
-        <Route exact path="/profil">
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movies/:id">
+            <MovieInfo />
+          </Route>
+          <Route exact path="/actor/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movie />
+          </Route>
+          <Route exact path="/profil">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 export default App;
