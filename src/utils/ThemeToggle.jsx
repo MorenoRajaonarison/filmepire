f1f24d4dc3c4dc0bb531d4e3a9a3ae510683a0ca
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-11-22 09:22:28 +0100

    fix: theme persistance

--- src/utils/ThemeToggle.jsx
+++ src/utils/ThemeToggle.jsx
@@ -1,15 +1,20 @@
-import { useState, useMemo, createContext } from "react";
+import { useState, useMemo, useEffect, createContext } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export const ThemeModeContext = createContext();
 
 const ThemeToggle = ({ children }) => {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState(localStorage.getItem("theme") || "light");
+
   const toggleColorMode = () => {
     setMode((prev) => (prev === "light" ? "dark" : "light"));
   };
 
   const theme = useMemo(() => createTheme({ palette: { mode } }), [mode]);
+
+  useEffect(() => {
+    localStorage.setItem("theme", mode);
+  }, [mode]);
 
   return (
     <ThemeModeContext.Provider value={{ mode, setMode, toggleColorMode }}>
